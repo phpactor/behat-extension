6231ea9d90abe71df808d9248b12685418d5e8ff
Daniel Leech <dan.t.leech@gmail.com>
2018-12-21 17:57:24 +0000

    support step defiition locator

--- lib/BehatExtension.php
+++ lib/BehatExtension.php
@@ -10,7 +10,9 @@ use Phpactor\Extension\Behat\Behat\BehatConfig;
 use Phpactor\Extension\Behat\Behat\StepGenerator;
 use Phpactor\Extension\Behat\Behat\StepParser;
 use Phpactor\Extension\Behat\Completor\FeatureStepCompletor;
+use Phpactor\Extension\Behat\ReferenceFinder\StepDefinitionLocator;
 use Phpactor\Extension\Completion\CompletionExtension;
+use Phpactor\Extension\ReferenceFinder\ReferenceFinderExtension;
 use Phpactor\Extension\WorseReflection\WorseReflectionExtension;
 use Phpactor\FilePathResolverExtension\FilePathResolverExtension;
 use Phpactor\MapResolver\Resolver;
@@ -52,6 +54,10 @@ class BehatExtension implements Extension
                 $container->get('behat.step_parser')
             );
         }, [ CompletionExtension::TAG_COMPLETOR => [ CompletionExtension::KEY_COMPLETOR_TYPES => [ 'cucumber' ]]]);
+
+        $container->register('behat.reference_finder.step_definition_locator', function (Container $container) {
+            return new StepDefinitionLocator($container->get('behat.step_generator'), $container->get('behat.step_parser'));
+        }, [ ReferenceFinderExtension::TAG_DEFINITION_LOCATOR => []]);
     }
 
     /**
---
+++ tests/Unit/BehatExtensionTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Phpactor\Extension\Behat\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+use Phpactor\ClassFileConverter\Domain\ClassToFileFileToClass;
+use Phpactor\Container\PhpactorContainer;
+use Phpactor\Extension\Behat\BehatExtension;
+use Phpactor\Extension\ClassToFile\ClassToFileExtension;
+use Phpactor\Extension\ComposerAutoloader\ComposerAutoloaderExtension;
+use Phpactor\Extension\Logger\LoggingExtension;
+use Phpactor\Extension\ReferenceFinder\ReferenceFinderExtension;
+use Phpactor\Extension\WorseReflection\WorseReflectionExtension;
+use Phpactor\FilePathResolverExtension\FilePathResolverExtension;
+use Phpactor\ReferenceFinder\DefinitionLocator;
+use Phpactor\TextDocument\ByteOffset;
+use Phpactor\TextDocument\TextDocumentBuilder;
+
+class BehatExtensionTest extends TestCase
+{
+    public function testStepDefinitionFinder()
+    {
+        $container = PhpactorContainer::fromExtensions([
+            BehatExtension::class,
+            ReferenceFinderExtension::class,
+            FilePathResolverExtension::class,
+            WorseReflectionExtension::class,
+            ClassToFileExtension::class,
+            ComposerAutoloaderExtension::class,
+            LoggingExtension::class,
+        ], [
+            'file_path_resolver.application_root' => __DIR__ . '/..'
+        ]);
+
+        $locator = $container->get(ReferenceFinderExtension::SERVICE_DEFINITION_LOCATOR);
+        $this->assertInstanceOf(DefinitionLocator::class, $locator);
+        $location = $locator->locateDefinition(
+            TextDocumentBuilder::fromUri(__DIR__. '/../Integration/Completor/feature/some_feature.feature')->language('cucumber')->build(),
+            ByteOffset::fromInt(69)
+        );
+
+        $this->assertContains('ExampleContext.php', $location->uri()->__toString());
+    }
+}
